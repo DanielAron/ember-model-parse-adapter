fbf168ec27e477225953c91e5fefc29a5a01817f
Sam Harnack <sam@harnackstudios.com>
2013-07-17 08:05:19 -0700

    added createRecord and saveRecord

--- ember-model-parse-adapter.js
+++ ember-model-parse-adapter.js
@@ -23,13 +23,43 @@ Ember.ParseAdapter = Ember.Adapter.extend({
     });
   },
 
-  _ajax: function(urlSuffix) {
-    var url = "https://api.parse.com/1/classes/" + urlSuffix;
-    return $.ajax(url, {
-      headers: {
-        "X-Parse-Application-Id": "ezGrks7YbAl62Cx8dj58H46iELdqNENdmHnjIciK",
-        "X-Parse-REST-API-Key": "b5iK2Y62SUv9325cbJcim7IuGiu7tZpCPZdSMWrk"
-      }
+  createRecord: function(record) {
+    var endpoint = classToEndpoint(record.constructor),
+      primaryKey = Ember.get(record.constructor, 'primaryKey'),
+      self = this;
+
+    return this._ajax(endpoint, record.toJSON(), "POST").then(function(data) {
+      self.didCreateRecord(record, data);
     });
+  },
+
+  didCreateRecord: function(record, data) {
+    var rootKey = get(record.constructor, 'rootKey'),
+        primaryKey = get(record.constructor, 'primaryKey'),
+        dataToLoad = rootKey ? data[rootKey] : data;
+
+    record.load(dataToLoad[primaryKey], dataToLoad);
+    record.didCreateRecord();
+  },
+
+  saveRecord: function(record) {
+    var primaryKey = Ember.get(record.constructor, 'primaryKey'),
+        id = Ember.get(record, primaryKey),
+        url = [ classToEndpoint(record.constructor), id ].join("/"),
+        self = this;
+
+    return this._ajax(url, record.toJSON(), "PUT").then(function(data) {  // TODO: Some APIs may or may not return data
+      self.didSaveRecord(record, data);
+    });
+  },
+
+  _ajax: function(urlSuffix, params, method) {
+    var url = "https://api.parse.com/1/classes/" + urlSuffix;
+    return $.ajax(url, $.extend({
+      headers: {
+        "X-Parse-Application-Id": "",
+        "X-Parse-REST-API-Key": ""
+      }
+    }, params), method || "GET");
   }
 });
